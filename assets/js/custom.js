6424c216727c46908f12eaf7b4b07870e544fe9f
jonathanzhang53 <jonathanzhang53@gmail.com>
2020-07-17 23:37:44 -0500

    most non-js added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,10 @@
 /* Custom JS
- * Written by Jonathan Zhang
+ * Written by Jonathan Zhang for jonathanzhang53.github.io
  */
+
+console.log("Please do not look too closely into the code for this website. It is pretty scrappy and beginner. Thanks! - Jonathan Zhang");
+
+function quoteVoice () {
+	var track = document.getElementById("quote-audio");
+	track.play();
+}
