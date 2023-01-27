49c4db973a1a05286fc2a36ea3b1a6e9b7fb07b1
jonathanzhang53 <jonathanzhang53@gmail.com>
2023-01-27 15:31:44 -0500

    contact form

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,3 +52,21 @@ window.addEventListener("scroll", function () {
     backTopBtn.classList.remove("active");
   }
 });
+
+/**
+ * copy text
+ */
+
+const email = document.querySelector("#email");
+
+email.onclick = function() {
+  document.execCommand("copy");
+}
+
+email.addEventListener("copy", function(event) {
+  event.preventDefault();
+  if (event.clipboardData) {
+    event.clipboardData.setData("text/plain", email.textContent);
+    // console.log(event.clipboardData.getData("text"))
+  }
+});
